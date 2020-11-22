ec8d0e8b9c78624aa5afbb09177fbbf810b51e6b
Darlin Artiles <darlin.ariles@gmail.com>
2020-11-21 17:03:16 -0700

    11212020

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from  'reactstrap';
+import CampsiteInfo from './CampsiteInfoComponent';
 import { render } from 'react-dom';
+
 
 class Directory extends Component {
     constructor(props) {
@@ -14,20 +16,7 @@ class Directory extends Component {
         this.setState({selectedCampsite: campsite});
     }
 
-    renderSelectedCampsite(campsite) {
-        if (campsite) {
-            return (
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        return <div />;
-    }
+
 
     render() {
         const directory = this.props.campsites.map(campsite => {
@@ -46,15 +35,10 @@ class Directory extends Component {
         return (
             <div className="container">
                 <div className="row">
-                {directory}
+                    {directory}
                 </div>
-                <div className="row">
-                    <div className="col-md-5 m-1">
-                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
-                    </div>
-                </div>
+            <CampsiteInfo campsite={this.state.selectedCampsite} />
             </div>
-            
         );
     }
 }
