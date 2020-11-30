ead87e80aab33e6a5884d268a530a90a7b957e01
Darlin Artiles <darlin.ariles@gmail.com>
2020-11-29 18:32:05 -0700

    11/29/2020

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 import 'typeface-lobster';
 import 'typeface-open-sans';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
+
 
 ReactDOM.render(
   <React.StrictMode>
