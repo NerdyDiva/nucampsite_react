abb328aeedcce7b651d84c925c3c5ecb0338b0ef
Darlin Artiles <darlin.ariles@gmail.com>
2020-12-08 16:11:23 -0700

    react redux validation

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -9,7 +9,7 @@ function RenderCard({item}) {
             <CardBody>
                 <CardTitle>{item.name}</CardTitle>
                 <CardText>{item.description}</CardText>
-            </CardBody>
+            </CardBody> 
         </Card>
     );
 }
